28224d3f165eb860067da472f662c0e1a9ef3ff8
ArekBerc <arekberc@gmail.com>
2019-12-28 19:50:24 +0300

    V2.3

--- geometric_approach/Arm.py
+++ geometric_approach/Arm.py
@@ -7,5 +7,5 @@ class Arm(object):
 		#self.limits =
 		self.joint_names=["UNKNOWN"]
 		# Joint names for specifying which joint has reached its limits
-		self.joint_angles = [0.0, pi/2, pi/2, 0.0, 0.0]
-		self.initial_angles = [0.0, pi/2, pi/2, 0.0, 0.0]
+		self.joint_angles = [0.0, pi/2, pi/2, 0.0, 0.0,0.0]
+		self.initial_angles = [0.0, pi/2, pi/2, 0.0, 0.0,0.0]
--- geometric_approach/geometrical_ik.py
+++ geometric_approach/geometrical_ik.py
@@ -10,33 +10,52 @@ from Arm import *
 x=0
 y=0
 z=0
+w=0
 def callback(data):
-	global x,y,z
-
-	xi=data.axes[1]*0.15
+	global x,y,z,w
+	xi=-data.axes[0]*0.15
 	yi=data.axes[3]*0.15
 	zi=-data.axes[2]*0.15
+	if data.buttons[4]==1:
+		#w=arm.initial_angles[4]-arm.joint_angles[4]
+		w=data.axes[1]
+		arm.joint_angles[4]=arm.joint_angles[4]+w*0.0085
+		x=x+xi
+		z=z+zi
+		y=y+yi
+		movement_2(arm,[54.0+x,50.0+y,0.0+z])
+
+	else:
+
+		w=arm.initial_angles[4]-arm.joint_angles[4]
 	#if(arm.joint_angles[1]>0.90 or xi<0):
-	x=x+xi
+		x=x+xi
 #	else:
 #		print("hiziniz 102 yavas amk")
 #	if(arm.joint_angles[3]>0.90 or yi<0):
-	y=y+yi
+		y=y+yi
 
 #	else:
 #		print("hiziniz 102 yavas amk")
+		z=z+zi
+		print(arm.joint_angles[1])
+		movement_1(arm,[54.0+x,50.0+y,0.0+z])
+	arm_pub = [arm.initial_angles[0]-arm.joint_angles[0],arm.initial_angles[1]-arm.joint_angles[1],arm.initial_angles[2]-arm.joint_angles[2],arm.initial_angles[3]-arm.joint_angles[3],arm.initial_angles[4]-arm.joint_angles[4],arm.initial_angles[5]-arm.joint_angles[5]]
+	print("s")
+	print(arm_pub[0])
+	print(arm_pub[1])
+	print(arm_pub[2])
+	print(arm_pub[3])
+	print(arm_pub[4])
+	print(arm_pub[5])
+	print("f")
 
-	z=z+zi
-	print(arm.joint_angles[1])
-	movement_1(arm,[54.0+x,50.0+y,0.0+z])
-	arm_pub = [arm.initial_angles[0]-arm.joint_angles[0],arm.initial_angles[1]-arm.joint_angles[1],arm.initial_angles[2]-arm.joint_angles[2],arm.initial_angles[3]-arm.joint_angles[3],arm.initial_angles[4]-arm.joint_angles[4]]
-	print(arm.joint_angles[1])
-	print(arm.joint_angles[3])
 	pub1.publish(arm_pub[0])
 	pub2.publish(arm_pub[1])
 	pub3.publish(arm_pub[2])
 	pub4.publish(arm_pub[3])
 	pub5.publish(arm_pub[4])
+	pub6.publish(arm_pub[5])
 
 
 def talker():
@@ -70,10 +89,18 @@ def movement_1(r,destination_point):
 	r.joint_angles[3]=-base_yaw
 
 # left right
-#def movement_2(length,destination_point):
-	#gripper roattion will be equal to the minus base rotation but
-	# you should consider the shortening of the range when the move happens
-
+def movement_2(r,destination_point):
+	distance=lengths(destination_point)
+	angles=get_triangle_angles(r.lengths[1],r.lengths[2] + r.lengths[4],distance)
+	alpha=atan(destination_point[1]/destination_point[0])
+	base_pitch=angles[1]+alpha
+	second_joint=angles[2]
+	a=sqrt( (distance ** 2)-(destination_point[1] ** 2))
+	base_yaw=asin(destination_point[2]/a)
+	#for making parallel
+	r.joint_angles[1]=base_pitch
+	r.joint_angles[2]=second_joint
+	r.joint_angles[0]=base_yaw
 
 
 if __name__ == '__main__':
